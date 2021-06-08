21779fb9a445f480ff1d615a2de1069c608d384a
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-06-08 19:54:23 +0200

    Removed unnecessary lines

--- skim-spots-app/navigation/index.tsx
+++ skim-spots-app/navigation/index.tsx
@@ -1,8 +1,3 @@
-/**
- * If you are not familiar with React Navigation, check out the "Fundamentals" guide:
- * https://reactnavigation.org/docs/getting-started
- *
- */
 import {
   NavigationContainer,
   DefaultTheme,
