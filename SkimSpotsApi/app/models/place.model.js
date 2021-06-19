97b54f9a5ae6e6b298579b388f862f2e46eea337
Konrad <konradlubera@hotmail.com>
2021-06-19 22:56:17 +0200

    Added path to images to place model

--- SkimSpotsApi/app/models/place.model.js
+++ SkimSpotsApi/app/models/place.model.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, Sequelize) => {
         },
         points: {
             type: Sequelize.INTEGER
+        },
+        pathToImages:{
+            type: Sequelize.STRING
         }
     });
 
