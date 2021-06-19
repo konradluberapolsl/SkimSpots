b83c1d85adcd1aa3a9cbf0cfbec0c96de94b32f6
Konrad <konradlubera@hotmail.com>
2021-06-19 19:07:03 +0200

    Edited comment model

--- SkimSpotsApi/app/models/comment.model.js
+++ SkimSpotsApi/app/models/comment.model.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, Sequelize) => {
     const Comment = sequelize.define("comment", {
-        points: {
+        content: {
             type: Sequelize.TEXT
         }
     });
