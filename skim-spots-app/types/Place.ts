efc9fb442b5806824b521f817b02d174a9633049
Konrad <konradlubera@hotmail.com>
2021-06-20 20:38:43 +0200

    Added place model

---
+++ skim-spots-app/types/Place.ts
@@ -0,0 +1,13 @@
+type Place = null | {
+    id: number;
+    name: string;
+    welcomeText: string;
+    information: string;
+    pathToImages: string;
+    points: number;
+    authorId: number;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export default Place;
