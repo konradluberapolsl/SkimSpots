a5d38cc046f869a9768e6a30a9e4c0b378a136e0
Konrad <konradlubera@hotmail.com>
2021-06-22 01:05:57 +0200

    Added header with logo

--- skim-spots-app/navigation/index.tsx
+++ skim-spots-app/navigation/index.tsx
@@ -6,13 +6,17 @@ import {
 } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as React from "react";
-import { ActivityIndicator, ColorSchemeName } from "react-native";
+import {ActivityIndicator, ColorSchemeName} from "react-native";
+import {Image} from "react-native-elements"
 import { AuthContext } from "../context/AuthContext";
 import NotFoundScreen from "../screens/NotFoundScreen";
 import { RootStackParamList } from "../types";
 import AuthenticationNavigator from "./AuthenticationNavigator";
 import BottomTabNavigator from "./BottomTabNavigator";
 import LinkingConfiguration from "./LinkingConfiguration";
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+
 
 const Navigation = ({ colorScheme }: { colorScheme: ColorSchemeName }) => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
@@ -34,12 +38,14 @@ const Navigation = ({ colorScheme }: { colorScheme: ColorSchemeName }) => {
   if (isLoading) {
     return <ActivityIndicator animating={true} color="Black" size="large" />;
   } else {
-    return (
+
+      return (
       <NavigationContainer
         linking={LinkingConfiguration}
         theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+
       >
-        {user ? <RootNavigator /> : <AuthenticationNavigator />}
+        {user ? <RootNavigator/> : <AuthenticationNavigator />}
       </NavigationContainer>
     );
   }
@@ -50,13 +56,28 @@ export default Navigation;
 const Stack = createStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    const colorScheme = useColorScheme();
+    return (
+    <Stack.Navigator screenOptions={{
+            //headerShown: false,
+        headerTitleAlign: 'center',
+        headerStyle: {
+            backgroundColor: Colors[colorScheme].foreground,
+        },
+        headerTitle: () => (
+            <Image
+                source={require('../assets/images/logo-text.png')}
+                style={{ width: 55 , height: 36, paddingStart: -5 }}
+            />
+        )
+    }}
+    >
       <Stack.Screen name="Root" component={BottomTabNavigator} />
       <Stack.Screen
         name="NotFound"
         component={NotFoundScreen}
-        options={{ title: "Oops!" }}
+        options={{ title: "Oops!",
+        }}
       />
     </Stack.Navigator>
   );
