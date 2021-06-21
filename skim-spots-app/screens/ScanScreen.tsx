7719b05ed62101b6e0ce253fcbb1e67360ad9844
Konrad <konradlubera@hotmail.com>
2021-06-22 01:06:51 +0200

    Changes to ScanScreen

--- skim-spots-app/screens/ScanScreen.tsx
+++ skim-spots-app/screens/ScanScreen.tsx
@@ -1,17 +1,23 @@
 import * as React from "react";
-import {StyleSheet, Button, Dimensions, TouchableOpacity} from "react-native";
-import { Text, View } from "../components/Themed";
+import {StyleSheet, Dimensions, TouchableOpacity} from "react-native";
+import { Text, View, Button } from "../components/Themed";
+import {View as DefaultView} from "react-native"
 import Place from "../types/Place";
 import {getPlaceByName} from "../api/getPlaceByName";
 import {getPlacesNames} from "../api/getPlacesNames";
 import {getUserByID} from "../api/getUserByID";
 import { BarCodeScanner,  BarCodeScannerResult } from 'expo-barcode-scanner';
-
+import { BlurView } from 'expo-blur';
 import {useState, useEffect} from "react";
 import {BarcodeMask} from "@nartc/react-native-barcode-mask";
+import useColorScheme from "../hooks/useColorScheme";
+import Colors from "../constants/Colors";
+import {Image} from "react-native-elements";
 
 
 const ScanScreen = () => {
+
+  const colorScheme = useColorScheme();
 
   const [placesNames, setPlacesName] = useState<string[]>([]);
   const [hasPermission, setHasPermission] = useState<any>(null);
@@ -27,11 +33,13 @@ const ScanScreen = () => {
 
     getPlacesNames().then((data) => setPlacesName(data))
 
+    console.log(colorScheme)
+
   }, []);
 
   useEffect(() => {
     if(place != null){
-      alert(place.welcomeText);
+      setSuccessScan(true);
     }
   },[place])
 
@@ -39,12 +47,9 @@ const ScanScreen = () => {
     if (!scanned) {
       const {type, data} = scanningResult;
       setScanned(true);
-      console.log(data.split(/\r?\n/))
       const text = data.split(/\r?\n/)
       if(text.length > 1){
         if (placesNames.includes(text[1])){
-          setSuccessScan(true);
-          alert("Udało się!");
           getPlaceByName(text[1]).then( res => setPlace(res));
         }
         else
@@ -62,14 +67,40 @@ const ScanScreen = () => {
     return <Text>No access to camera</Text>;
   }
 
+  const dialogTextColor = Colors[colorScheme].secondaryText;
+
 
   return (
       <View style={styles.container}>
+
+
         <BarCodeScanner
             onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            style={StyleSheet.absoluteFillObject}
+            style={[StyleSheet.absoluteFillObject,]}
         />
-        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+
+        {scanned &&
+        <Button
+            style={[{backgroundColor: Colors[colorScheme].background} , styles.scanAgainButton]}
+            onPress={() =>{
+              setScanned(false);
+              setSuccessScan(false);
+              }}>
+          Scan Again
+        </Button>}
+        {successScan &&
+        <View
+            style={[{backgroundColor: Colors[colorScheme].background+"9F"},styles.resultDialog]}>
+          <Image
+              source={ colorScheme=="dark" ? require('../assets/images/logo-light.png'): require('../assets/images/logo-dark.png')}
+              style={{ width: 40 , height: 60, marginBottom: 20 }}
+          />
+          <Text style={[{color: dialogTextColor}, styles.resultDialogText]}>Za ten spot otrzymujesz:</Text>
+          <Text style={[{color: dialogTextColor}, styles.resultDialogText]}>{place!!.points} ptk</Text>
+          <Text style={[{color: dialogTextColor, marginTop: 100},styles.resultDialogText]} >{place!!.welcomeText}</Text>
+          <Button style={styles.goButton}>GO!</Button>
+        </View> }
+
       </View>
   );
 };
@@ -86,9 +117,29 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  resultDialog:{
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 75,
+    paddingHorizontal: 50,
+    borderRadius: 15,
+    width: '90%',
+    height: '75%',
+
   },
+  resultDialogText:{
+    fontSize: 24,
+    textAlign: "center"
+  },
+  scanAgainButton:{
+    position: "absolute",
+    bottom: 5,
+    width: "45%",
+  },
+  goButton:{
+    width: '80%',
+    marginTop: '20%'
+  }
+
 });
