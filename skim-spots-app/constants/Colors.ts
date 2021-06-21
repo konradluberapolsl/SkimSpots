007fd0c921a7e7e516d15d8a31201cfa08f65ba0
Konrad <konradlubera@hotmail.com>
2021-06-22 01:04:19 +0200

    Added new colors

--- skim-spots-app/constants/Colors.ts
+++ skim-spots-app/constants/Colors.ts
@@ -3,15 +3,19 @@ const tintColorDark = '#fff';
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: '#2B2D53',
+    secondaryText: '#000',
+    foreground: '#807585',
+    background: '#FFF4ED',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: '#F5C39B',
+    secondaryText: '#fff',
+    background: '#1E1D3F',
+    foreground: '#2B2D53',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
