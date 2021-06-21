bda79705cc6700d392fd4b34946edf608937f349
Konrad <konradlubera@hotmail.com>
2021-06-22 01:05:41 +0200

    Changed bottom tab nav to material version, icon assigment, and header configuration

--- skim-spots-app/navigation/BottomTabNavigator.tsx
+++ skim-spots-app/navigation/BottomTabNavigator.tsx
@@ -1,5 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {createMaterialBottomTabNavigator} from "@react-navigation/material-bottom-tabs"
 import { createStackNavigator } from "@react-navigation/stack";
 import * as React from "react";
 import Colors from "../constants/Colors";
@@ -18,53 +19,60 @@ import ScanScreen from "../screens/ScanScreen";
 import { Text, TouchableOpacity } from "react-native";
 import { AuthContext } from "../context/AuthContext";
 
-const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+const BottomTab = createMaterialBottomTabNavigator<BottomTabParamList>();
+
 
 const BottomTabNavigator = () => {
-  const colorScheme = useColorScheme();
-
+    const colorScheme = useColorScheme();
   return (
     <BottomTab.Navigator
       initialRouteName="Profile"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+
+      screenOptions={
+          ({ route }) => ({
+              tabBarIcon: ({ focused, color}) => {
+                  let iconName;
+
+                  if (route.name == 'Profile') {
+                      iconName = 'person';
+                  } else if (route.name == 'Home') {
+                      iconName = 'home';
+                  } else if (route.name == 'Scan') {
+                      iconName = 'scan-outline';
+                  } else if(route.name == 'Settings')
+                      iconName = 'settings'
+
+                  // You can return any component that you like here!
+                  // @ts-ignore
+                  return <Ionicons name={iconName} size={23} color={color}  />;
+              },
+
+          })}
+      barStyle={{ backgroundColor: Colors[colorScheme].foreground }}
     >
       <BottomTab.Screen
         name="Profile"
         component={ProfileNavigator}
         options={{
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="ios-code" color={color} />
-          ),
+
         }}
       />
       <BottomTab.Screen
         name="Home"
         component={HomeNavigator}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="ios-code" color={color} />
-          ),
-        }}
       />
 
       <BottomTab.Screen
         name="Scan"
         component={ScanNavigator}
         options={{
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="ios-code" color={color} />
-          ),
         }}
       />
 
       <BottomTab.Screen
         name="Settings"
         component={SettingsNavigator}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="ios-code" color={color} />
-          ),
-        }}
+        options={{}}
       />
     </BottomTab.Navigator>
   );
@@ -72,33 +80,32 @@ const BottomTabNavigator = () => {
 
 export default BottomTabNavigator;
 
-const TabBarIcon = (props: {
-  name: React.ComponentProps<typeof Ionicons>["name"];
-  color: string;
-}) => {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-};
 
 const ProfileStack = createStackNavigator<ProfileParamList>();
 
 const ProfileNavigator = () => {
   const { logout } = React.useContext(AuthContext);
-
+    const colorScheme = useColorScheme();
   return (
     <ProfileStack.Navigator>
       <ProfileStack.Screen
         name="ProfileScreen"
         component={ProfileScreen}
         options={{
-          headerTitle: "Profile Title",
+            headerShown: false,
+            headerTitle: "Profile Title",
+            headerStyle: {
+                backgroundColor: Colors[colorScheme].foreground,
+            },
           headerRight: () => {
             return (
               <TouchableOpacity
+                style={{marginEnd:20}}
                 onPress={() => {
                   logout();
                 }}
               >
-                <Text>LOGOUT</Text>
+                <Text style={{color: Colors[colorScheme].text}}>LOGOUT</Text>
               </TouchableOpacity>
             );
           },
@@ -111,12 +118,19 @@ const ProfileNavigator = () => {
 const HomeStack = createStackNavigator<HomeParamList>();
 
 const HomeNavigator = () => {
+    const colorScheme = useColorScheme();
   return (
     <HomeStack.Navigator>
       <HomeStack.Screen
         name="HomeScreen"
         component={HomeScreen}
-        options={{ headerTitle: "Home Title" }}
+        options={{
+            headerShown: false,
+            headerTitle: "Home Title",
+            headerStyle: {
+                backgroundColor: Colors[colorScheme].foreground,
+            },
+        }}
       />
     </HomeStack.Navigator>
   );
@@ -125,12 +139,20 @@ const HomeNavigator = () => {
 const ScanStack = createStackNavigator<ScanParamList>();
 
 const ScanNavigator = () => {
+    const colorScheme = useColorScheme();
+
   return (
     <ScanStack.Navigator>
       <ScanStack.Screen
         name="ScanScreen"
         component={ScanScreen}
-        options={{ headerTitle: "Scan Title" }}
+        options={{
+            headerShown: false,
+            headerTitle: "Scan Title",
+            headerStyle: {
+                backgroundColor: Colors[colorScheme].foreground,
+            },
+        }}
       />
     </ScanStack.Navigator>
   );
@@ -139,12 +161,19 @@ const ScanNavigator = () => {
 const SettingsStack = createStackNavigator<SettingsParamList>();
 
 const SettingsNavigator = () => {
+    const colorScheme = useColorScheme();
   return (
     <SettingsStack.Navigator>
       <SettingsStack.Screen
         name="SettingsScreen"
         component={SettingsScreen}
-        options={{ headerTitle: "Settings Title" }}
+        options={{
+            headerShown: false,
+            headerTitle: "Settings Title",
+            headerStyle: {
+                backgroundColor: Colors[colorScheme].foreground,
+            },
+        }}
       />
     </SettingsStack.Navigator>
   );
