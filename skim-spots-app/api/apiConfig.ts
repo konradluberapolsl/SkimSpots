a710af09a4092620e38491095aae0c14febae1c9
Konrad <konradlubera@hotmail.com>
2021-06-25 01:35:15 +0200

    Edited api config component to handle post request

--- skim-spots-app/api/apiConfig.ts
+++ skim-spots-app/api/apiConfig.ts
@@ -3,10 +3,10 @@ const yourLocalIP : string = "192.168.0.80"
 
 const url: string = `http://${yourLocalIP}:8000/api`
 
-export async function client( endpoint: string, query: string) : Promise<any> {
-    return await fetch(`${url}/${endpoint}/${query}`)
+export async function client( endpoint: string, query: string, options = {}) : Promise<any> {
+
+    return await fetch(`${url}/${endpoint}/${query}`, options)
         .then((res) => {
-            console.log(`${url}/${endpoint}/${query}`)
             if(res.ok) return res.json();
             if(!res.ok) return Promise.reject(`Http error: ${res.status}`);
         }).catch((error) => {
