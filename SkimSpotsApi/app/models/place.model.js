c4a726cf7dfab5980930623b2905aa2b819845bd
Konrad <konradlubera@hotmail.com>
2021-06-25 01:28:17 +0200

    Added estimated localization to place model.

--- SkimSpotsApi/app/models/place.model.js
+++ SkimSpotsApi/app/models/place.model.js
@@ -6,6 +6,9 @@ module.exports = (sequelize, Sequelize) => {
         welcomeText: {
             type: Sequelize.STRING
         },
+        estimatedLocalization:{
+          type: Sequelize.STRING
+        },
         information: {
             type: Sequelize.TEXT
         },
