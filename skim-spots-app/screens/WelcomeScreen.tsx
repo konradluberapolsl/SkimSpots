3b5b60d1cdb8e7f24bf5fc4b78c1740b5092339b
Konrad <konradlubera@hotmail.com>
2021-06-27 02:32:50 +0200

    Styled welcome screen

--- skim-spots-app/screens/WelcomeScreen.tsx
+++ skim-spots-app/screens/WelcomeScreen.tsx
@@ -1,15 +1,69 @@
 import * as React from "react";
-import { Button, StyleSheet } from "react-native";
-import { Text, View } from "../components/Themed";
+import {
+    Image,
+    StyleSheet,
+    KeyboardAvoidingView,
+    Platform,
+    SafeAreaView,
+    TouchableWithoutFeedback,
+    Keyboard,
+
+} from "react-native";
+import {Text, View, Button,  ScrollView } from "../components/Themed";
 import { AuthContext } from "../context/AuthContext";
+import { Input } from 'react-native-elements';
+import {Ionicons} from "@expo/vector-icons";
+import useColorScheme from "../hooks/useColorScheme";
+import Colors from "../constants/Colors";
+import Layout from "../constants/Layout";
+
+
 
 const WelcomeScreen = () => {
-  const { register } = React.useContext(AuthContext);
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome</Text>
-      <Button title={"Go!"} onPress={() => register()} />
-    </View>
+    const colorScheme = useColorScheme();
+
+    const [height, setHeight] = React.useState();
+
+    const [text, setText] = React.useState("");
+
+    const { register } = React.useContext(AuthContext);
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : "height"}
+            style={{flex: 1}}
+            contentContainerStyle={{flex: 1}}
+        >
+        <ScrollView style={{flex:1}} contentContainerStyle={styles.container}>
+            <Image
+                source={ colorScheme == "dark" ? require("../assets/images/logo-text-icon-dark.png") : require("../assets/images/logo-text-icon-light.png") }
+                resizeMethod="auto"
+                resizeMode="contain"
+                style={{width:"45%", height:"15%", marginTop: '70%'}}
+            />
+
+          <Input
+              value={text}
+              onChangeText={setText}
+              containerStyle={styles.inputContainer}
+              inputContainerStyle={[{backgroundColor: Colors[colorScheme].foreground, borderRadius: 30, paddingHorizontal: 10  , borderBottomWidth: 0 }]}
+              inputStyle={{color: '#FFF'}}
+              placeholder='Twój nick..'
+              rightIcon={
+                <Ionicons name={"pencil"} size={23} color='white'  />
+              }
+          />
+
+          <Button disabled={text==""}  style={styles.button} onPress={() => register(text)}>GO!</Button>
+
+            <Image
+                source={colorScheme == "dark" ? require("../assets/images/waves-dark.png") : require("../assets/images/waves-light.png") }
+                resizeMethod="resize"
+                resizeMode="contain"
+                style={[styles.image, {width:"100%", height: (0.4 * Layout.window.height) }]}
+            />
+        </ScrollView>
+        </KeyboardAvoidingView>
+
   );
 };
 
@@ -19,15 +73,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start"
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  button:{
+    position: "absolute",
+    bottom: 60,
+    width: "50%",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  inputContainer:{
+      width: "70%",
+      color: "#FFF",
+      marginTop: 50
   },
+  image:{
+    position: "absolute",
+    bottom: -10,
+    zIndex: -1
+  }
+
+
 });
