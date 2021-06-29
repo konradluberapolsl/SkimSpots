d1d3f9d904428280c2bd6a78ab6719e1576ecd69
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-06-29 08:52:51 +0200

    Create ranking of users

--- skim-spots-app/screens/RankingScreen.tsx
+++ skim-spots-app/screens/RankingScreen.tsx
@@ -1,45 +1,107 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
-import { Text, View } from "../components/Themed";
+import { 
+    FlatList, 
+    StyleSheet, 
+    Image } from "react-native";
+import { 
+    getUserPoints } from "../api/getUserPoints";
+import { 
+    Text, 
+    View } from "../components/Themed";
+import { 
+    AuthContext } from "../context/AuthContext";
 import UserPoints from "../types/UserPoints";
-
-
-interface RankingScreenProps{
-    ranking: UserPoints[],
-}
 
 interface OwnProps{
     route: any,
     navigation: any
 }
 
-
 const RankingScreen = (props: OwnProps) => {
 
-    const { ranking }: RankingScreenProps = props.route.params;
+    const { user } = React.useContext(AuthContext);
+    const [ranking, setRanking] = React.useState<UserPoints[]>([]);
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Ranking</Text>
+    const image = 'https://cdn.iconscout.com/icon/free/png-512/account-269-866236.png';
+
+    React.useEffect(() => {
+        getUserPoints().then(r => {
+            setRanking(r);
+        })
+    },[])
+
+    return(
+        <View style={{ flex: 1 }} >
+          <FlatList
+          data={ranking}
+          horizontal={false}
+          keyExtractor={item => item!!.id.toString()}
+          renderItem={(post) => {
+            const item = post.item;
+            return (
+                  <View style={styles.row}>
+                    <Image style={styles.pic} source={{uri:image}}></Image>
+                    <View>
+                      <View style={styles.nameContainer}>
+                        {item?.user?.id == user?.id &&
+                            <Text style={styles.nameTxtActive} numberOfLines={1} ellipsizeMode="tail">{item?.user?.name}</Text>
+                        }
+                        {item?.user?.id != user?.id &&
+                            <Text style={styles.nameTxt} numberOfLines={1} ellipsizeMode="tail">{item?.user?.name}</Text>
+                        }
+                        <Text style={styles.mblTxt}>{item?.amount} pkt</Text>
+                      </View>
+                    </View>
+                  </View>
+              );
+          }}/>
         </View>
     );
-};
+}
 
 export default RankingScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      borderColor: '#DCDCDC',
+      borderBottomWidth: 1,
+      height: 75,
     },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
+    pic: {
+      borderRadius: 30,
+      width: 60,
+      height: 60,
     },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
+    nameContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: 300,
     },
-});
+    nameTxt: {
+      marginLeft: 15,
+      fontWeight: '600',
+      fontSize: 22,
+      width:170,
+    },
+    nameTxtActive: {
+        marginLeft: 15,
+        fontWeight: 'bold',
+        fontSize: 25,
+        width:170,
+      },
+    mblTxt: {
+      fontWeight: '200',
+      fontSize: 17,
+    },
+    msgContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    msgTxt: {
+      fontWeight: '400',
+      fontSize: 16,
+      marginLeft: 15,
+    },
+  });
