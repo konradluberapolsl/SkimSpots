c01185a13276b723deb5863d2054cd5860dd6a10
Remigiusz Drobinski <30534425+curiosis@users.noreply.github.com>
2021-06-29 08:52:11 +0200

    Create premium places screen

--- skim-spots-app/screens/HomeScreen.tsx
+++ skim-spots-app/screens/HomeScreen.tsx
@@ -1,30 +1,104 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
-import { Text, View } from "../components/Themed";
+import { 
+  getCurrentPremiumPlaces } from "../api/getCurrentPremiumPlaces";
+import { 
+  Text, 
+  View } from "../components/Themed";
+import {
+  StyleSheet,
+  Image,
+  FlatList,
+  Pressable,
+} from 'react-native';
+import { 
+  ThemeContext } from "../context/ThemeContext";
+import Colors from "../constants/Colors";
+import PremiumPlace from "../types/PremiumPlace";
+import PremiumPlaceItem from "../components/PremiumPlaceItem";
 
-const HomeScreen = () => {
-  return (
+interface OwnProps {
+  route: any;
+  navigation: any;
+}
+
+const HomeScreen = (props: OwnProps) => {
+  const { REACT_APP_TEST_KEY } = process.env;
+  const yourLocalIP: string =
+    REACT_APP_TEST_KEY !== undefined ? REACT_APP_TEST_KEY : "192.168.1.22";
+  const [premiumPlaces, setPremiumPlaces] = React.useState<PremiumPlace[]>([]);
+
+  React.useEffect(() => {
+    getCurrentPremiumPlaces().then(r => {
+        setPremiumPlaces(r);
+    })
+},[])
+
+const [blur, setBlur] = React.useState(0);
+
+return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-    </View>
-  );
-};
-
+        <Text style={styles.title}>Today's power spots.</Text>
+        <Text style={styles.subtitle}>Find those spots and get extra points!</Text>
+        <FlatList style={styles.list}
+          contentContainerStyle={styles.listContainer}
+          data={premiumPlaces}
+          horizontal={false}
+          keyExtractor={item => item!!.id.toString()}
+          renderItem={(post) => {
+            const item = post.item;
+            return (
+              <View style={styles.card}>
+                <Pressable onPressIn={() => {setBlur(15)}} onPressOut={() => {setBlur(0)}} >
+                  <Image style={styles.cardImage} source={{uri:'https://lorempixel.com/400/200/nature/4/'}} blurRadius={blur}></Image>
+                </Pressable>
+                  {blur != 0 && <Text style={{fontWeight:'bold', fontSize:28, position: 'absolute',textAlign: "center",}}>{item?.place?.name}</Text>}
+                  {blur !=0 && <Text style={{fontSize:35, position: 'absolute',paddingTop: 120,}}>{item?.place?.points}+{item?.premiumPoint} pkt</Text>}
+              </View>
+            )
+          }}/>
+      </View>
+);
+}
+  
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  container:{
+    flex:1,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  list: {
+    paddingHorizontal: 5,
+  },
+  listContainer:{
+    alignItems:'center',
   },
   separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+    marginTop: 10,
   },
-});
+  card:{
+    marginVertical: 8,
+    paddingTop: 20,
+    flexBasis: '47%',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    textAlign: "center",
+  },
+  cardImage:{
+    flex: 1,
+    height: 250,
+    width: 350,
+    borderRadius: 50,
+  },
+  title:{
+    fontSize:35,
+    textAlign: "center",
+    fontWeight: 'bold',
+  },
+  subtitle:{
+    fontSize:20,
+    textAlign: "center",
+    paddingTop:10,
+  },
+}); 
